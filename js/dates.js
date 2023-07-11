cf77cbaae1100f3b2467d68fa143cca97ba12fbc
bruno <brucapriz@icloud.com>
2023-07-11 20:25:31 -0300

    avances

---
+++ js/dates.js
@@ -0,0 +1,116 @@
+const snkrs = [
+    {
+      brand: 'Nike',
+      model: 'Jordan 1',
+      price: 30000,
+      color:'Blanco'
+    },
+    {
+      brand: 'Adidas',
+      model: 'All Star',
+      price: 40000,
+      color: 'Negro',
+    },
+    {
+      brand: 'New Balance',
+      model: '550',
+      price: 20000,
+      color: 'Blanco',
+    },
+    {
+      brand: 'Adidas',
+      model: 'Forum Low',
+      price: 35000,
+      color: 'Negro',
+    },
+    {
+      brand: 'Nike',
+      model: 'Jordan',
+      price: 70000,
+      color: 'Rojo',
+    },
+    {
+      brand: 'Puma',
+      model: 'Slipstream',
+      price: 25000,
+      color: 'Blanco',
+    },
+    {
+      brand: 'New Balance',
+      model: '550',
+      price: 60000,
+      color: 'Gris',
+    },
+    {
+      brand: 'New Balance',
+      model: '550',
+      price: 80000,
+      color: 'Rojo',
+    },
+    {
+      brand: 'Vans',
+      model: 'Old Skool',
+      price: 40000,
+      color: 'Blanco',
+    },
+    {
+      brand: 'Adidas',
+      model: 'Yeezy',
+      price: 55000,
+      color: 'Negro',
+    },
+    {
+      brand: 'Vans',
+      model: 'Knu Skool',
+      price: 25000,
+      color: 'Rojo',
+    },
+    {
+      brand: 'Puma',
+      model: 'Classic',
+      price: 45000,
+      color: 'Azul',
+    },
+    {
+      brand: 'Nike',
+      model: 'Up Tempo',
+      price: 90000,
+      color:'Blanco',
+    },
+    {
+      brand: 'New Balance',
+      model: '585',
+      price: 60000,
+      color: 'Negro',
+    },
+    {
+      brand: 'Vans',
+      model: 'Old Skool',
+      price: 35000,
+      color: 'Azul',
+    },
+    {
+      brand: 'Nike',
+      model: 'Jordan',
+      price: 50000,
+      color: 'Blanco',
+    },
+    {
+      brand: 'Nike',
+      model: 'Air Max',
+      price: 80000,
+      color: 'Negro',
+    },
+    {
+      brand: 'Puma',
+      model: 'Classic',
+      price: 40000,
+      color: 'Blanco',
+    },
+    {
+      brand: 'Adidas',
+      model: 'Forum Low',
+      price: 30000,
+      color: 'Azul'
+      }
+  ]
--- js/main.js
+++ js/main.js
@@ -1,12 +1,114 @@
+const date = new Date();
+const hour = date.getHours();
+const nameWelcome = prompt ("Ingrese su nombre");
+
+function getInformationForFilter() {
+  let brand = prompt("¿Qué marca de calzado estás buscando?");
+  let priceMin = prompt("Coloca el precio mínimo que deseas");
+  let priceMax = prompt("Coloca el precio máximo que deseas");
+  let color = prompt("¿Qué color deseas buscar?");
+  }
+  
+function greeting (hour) {
+if (hour < 12) {
+  greeting = console.log ("¡Buenos dias " + nameWelcome + "!"),
+  document.write ("¡Buenos dias " + nameWelcome + "!");
+} else if (hour < 18) {
+  greeting = console.log ("¡Buenas tardes " + nameWelcome + "!"),
+  document.write ("¡Buenas tardes " + nameWelcome + "!");
+} else {
+  greeting = console.log ("¡Buenas noches " + nameWelcome + "!")
+  document.write ("¡Buenas noches " + nameWelcome + "!");
+} }
+
+
+/**********************************************************************/
+
+
+const searchData = {
+  brand: brand,
+  model: '',
+  priceMin: priceMin,
+  priceMax: priceMax,
+  color: color,
+}
+
+function showSnkrs(snkrs) {
+  snkrs.forEach(snkrs => {
+    console.log( "Marca: " + snkrs.brand + " | Modelo: " + snkrs.model + " |Color: " + snkrs.color + " |Precio: $ " + snkrs.price)
+
+    document.write("<p>" + "Marca: " + snkrs.brand + " | Modelo: " + snkrs.model + " |Color: " + snkrs.color + " |Precio: $ " + snkrs.price + "</p>")
+
+  })
+}
+
+function noResults() {
+  console.log("No hay Resultados")
+  document.write("<p>No hay Resultados</p>")
+}
+
+
+function filterBrand(snkrs) {
+  if (searchData.brand) {
+    return snkrs.brand == searchData.brand
+  }
+  return snkrs;
+}
+
+function filterModel(snkrs) {
+  if (searchData.model) {
+    return snkrs.model == searchData.model
+  }
+  return snkrs;
+}
+
+function filterMin(snkrs) {
+  if (searchData.priceMin) {
+    return snkrs.price >= searchData.priceMin
+  }
+  return snkrs;
+}
+
+function filterMax(snkrs) {
+  if (searchData.priceMax) {
+    return snkrs.price <= searchData.priceMax
+  }
+  return snkrs;
+}
+
+
+
+function filterColor(snkrs) {
+  if (searchData.color) {
+    return snkrs.color == searchData.color
+  }
+  return snkrs;
+}
+
+
+function filterSnkrs() {
+  getInformationForFilter();  
+  let result = snkrs.filter(filterBrand).filter(filterModel).filter(filterMin).filter(filterMax).filter(filterColor);
+
+
+  if (result.length) {
+    showSnkrs(result)
+  } else {
+    noResults()
+  }
+}
+
+
 function start() {
-    let op = prompt("¡Bienvenido!\nIngrese el numero de opción elegida:\n\n   1: Ingresar\n   2: Registrarse\n\n   3: Salir");
+   greeting();
+    let op = prompt("Ingrese el numero de opción elegida:\n\n   1: Ver productos disponibles \n   2: Filtrar busqueda \n\n   3: Salir");
 
     switch(op){
         case "1":
-            logIn();
+           showSnkrs(snkrs);
             break;
         case "2":
-            register(); 
+            filterSnkrs(); 
             break;
         case "3":
             alert('Hasta luego');
